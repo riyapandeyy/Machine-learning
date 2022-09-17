bdf364801bcc52e8a44d9afd7ef4ed0be3f3685d
Riya Pandey <riyapandey6849@gmail.com>
2022-09-17 21:44:12 +0530

    Create else.cpp

---
+++ else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int savings;
+cin>>savings;
+
+if(savings>5000) {
+    cout<<"Aashi\n";
+}  else{
+      cout<<"Varnika\n";
+}        
+  
+  return 0;
+
+}
